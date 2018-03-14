90f2fdc19adcacc1fccae5aed8fa914bd3b35446
Ani Adhikary <33363003+Ani-Adhikary@users.noreply.github.com>
2018-03-14 18:52:49 +0530

    Update Employee.h

--- ObjCBasicsDemo/Employee.h
+++ ObjCBasicsDemo/Employee.h
@@ -6,16 +6,18 @@
 //  Copyright © 2018 Ani Adhikary. All rights reserved.
 //
 
-// The interface section contains the instance variables and method declarations
+// The interface section contains the instance variables and method declarations.
 
 #import <Foundation/Foundation.h>
 
 @interface Employee : NSObject {
+    //instance variable declarations
     int _empId;
     double _salary;
     NSString *_empName;
 }
 
+//Method Declarations
 //Accessors - Getters and Setters
 -(void) setEmpId: (int) empIdVal;
 -(int) empId;
